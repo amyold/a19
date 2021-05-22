25eb843c1032a75fe520dcf2f7caffaf06bd524b
cym-cell <73395175+cym-cell@users.noreply.github.com>
2021-05-22 20:55:22 +0800

    Revert "[update]"
    This reverts commit 1e2996ea21a888066294dbfe9b23af7751d398c1.

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,24 @@
 <template>
   <div id="app">
-    <el-container v-show="!(path === '/login')">
-      <el-aside><navigation /></el-aside>
-      <el-container>
-        <el-header height="40px"><topBar /></el-header>
-        <el-main><router-view></router-view></el-main>
-      </el-container>
-    </el-container >
-    <router-view v-show="(path === '/login')"></router-view>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import topBar from "@components/topBar.vue";
-import navigation from '@components/navigation.vue';
-export default {
-  name: "app",
-  components: {
-    navigation,
-    topBar,
-  },
-  data() {
-    Navigation
-    return {
-      path: "",
-    }
-  },
-  mounted(){
-    this.path=this.$route.path
-  },
-  watch: {
-    $route(to,from){
-      this.path=to.path;
-    }
-  }
-};
-</script>
 <style>
 * {
   margin: 0;
   padding: 0;
   font-family: "微软雅黑";
 }
+
 html,body,#app {
   height: 100%;
   width: 100%;
+  display: flex;
 }
-#container {
-  height: 100%;
-  width: 100%;
-}
+
 body {
   background-color: #f8f9fc;
 }
-.el-container .el-main {
-  padding: 0;
-}
+
 </style>
---
+++ src/components/finished.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="container">
+    <div class="datalist">
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        tooltip-effect="dark"
+        style="width: 1320px"
+        :header-cell-style="{ 'text-align': 'center', 'font-size': '12px', }"
+        :cell-style="{
+          'text-align': 'center',
+          'padding-top': '20px',
+          'font-size': '12px',
+        }"
+        :row-style="{ height: '100px' }"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column prop="number" label="序号" width="45">
+        </el-table-column>
+        <el-table-column prop="productPic" label="商品图片" width="120">
+          <template slot-scope="scope">
+            <el-image :src="scope.row.productPic"></el-image>
+          </template>
+        </el-table-column>
+        <el-table-column prop="productName" label="商品名" width="120">
+        </el-table-column>
+        <el-table-column prop="dayNumber" label="已寄存天数" width="120">
+        </el-table-column>
+        <el-table-column prop="cost" label="寄存单价" width="75">
+        </el-table-column>
+        <el-table-column prop="address" label="收货地址" width="180">
+        </el-table-column>
+        <el-table-column
+          prop="clientInfo"
+          label="收货人/联系电话"
+          width="180"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="bussinessInfo"
+          label="商家店铺/联系电话"
+          width="180"
+        >
+        </el-table-column>
+        <el-table-column prop="totalCost" label="总费用" width="70">
+        </el-table-column>
+        <el-table-column prop="state" label="状态" show-overflow-tooltip>
+        </el-table-column>
+
+      </el-table> 
+    </div>
+    <div class="page">
+        <el-pagination
+          background
+          layout="total, prev, pager, next"
+          :total="count"
+        >
+        </el-pagination>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "finished",
+  data() {
+    return {
+      value1: true,
+      count: 3,
+      tableData: [
+        {
+          number: 1,
+          productPic: "https://s3.ax1x.com/2021/02/23/yLu6eS.jpg",
+          productName: "超级马里奥",
+          dayNumber: "10天",
+          cost: "10元/天",
+          address: "某某省某某市某某大学城",
+          clientInfo: "minus/18559120521",
+          bussinessInfo: "minus/18559120521",
+          totalCost: '100元',
+          state: "已完成"
+        },
+        {
+          number: 2,
+          productPic: "https://s3.ax1x.com/2021/02/23/yLuWJs.jpg",
+          productName: "塞尔达传说",
+          dayNumber: "16天",
+          cost: "12元/天",
+          address: "某某省某某市某某大学城",
+          clientInfo: "plus/18559120521",
+          bussinessInfo: "minus/18559120521",
+          totalCost: '192元',
+          state: "已完成"
+        },
+        {
+          number: 1,
+          productPic: "https://s3.ax1x.com/2021/02/23/yLu6eS.jpg",
+          productName: "超级马里奥",
+          dayNumber: "10天",
+          cost: "10元/天",
+          address: "某某省某某市某某大学城",
+          clientInfo: "minus/18559120521",
+          bussinessInfo: "minus/18559120521",
+          totalCost: '100元',
+          state: "已完成"
+        }
+      ],
+      multipleSelection: [],
+    };
+  },
+  methods: {
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach((row) => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      } else {
+        this.$refs.multipleTable.clearSelection();
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+  },
+};
+</script>
+<style scoped>
+.datalist {
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+}
+
+.datalist .page {
+  margin-top: 24px;
+}
+
+.el-button--primary {
+  height: 32px;
+  font-size: 2px;
+  border-radius: 8px;
+  border: none;
+}
+
+.edit {
+  background-color: #957bf1;
+}
+
+.edit:hover {
+  background-color: #a38ef4;
+}
+
+.el-pagination {
+  padding-left: 1140px;
+  padding-top: 20px;
+}
+</style>
